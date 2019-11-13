2263cbb395a108bb2bd25cef7983928f8da54819
unknown <900161@proway.treina>
2019-11-13 09:29:27 -0300

    Aula4

---
+++ Aula4/Aula4.py
@@ -0,0 +1,18 @@
+idade = 25
+
+if idade < 18:
+    print ('Dimenor')
+
+elif idade == 18:
+    print ('Silasco')
+
+else:
+    print ('SilascoMaisAinda')
+
+
+ativo = True
+
+if ativo:
+    print ('Pode Logar')
+else:
+    print ('Não Pode')
