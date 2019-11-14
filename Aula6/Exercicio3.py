f395e2f5bceeecbf1ac7f3fc2d2c21ded66630d5
unknown <900161@proway.treina>
2019-11-14 07:54:22 -0300

    Aula6

--- Aula6/Exercicio3.py
+++ Aula6/Exercicio3.py
@@ -1,19 +1,23 @@
-lista_alunos []
+aluno = []
+nota1 = []
+nota2 = []
+nota3 = []
+nota4 = []
 
-for i in range(0,10):
-    nome = input(f'Nome do Aluno {i}: ')
-    lista.aluno.append(nome)
-
-print(lista_aluno)
-
-'''
 for i in range (0,10):
-    nomes = input("Digite o nome do aluno: "), int(input("Digite a primeira nota: ")), int(input("Digite a segunda nota: ")), int(input("Digite a terceira nota: ")))
+    aluno.append(input("Digite o nome do aluno: "))
+    nota1.append(int(input("Digite a primeira nota: ")))
+    nota2.append(int(input("Digite a segunda nota: ")))
+    nota3.append(int(input("Digite a terceira nota: ")))
     nota4.append(int(input("Digite a quarta nota: ")))
 
-print (nomes, nota1, nota2, nota3, nota4)
+for i in range (0,10):
+    print ('\nNome do Aluno:', aluno[i],'\nNotas:', nota1[i], nota2[i], nota3[i], nota4[i])
+    media = ((nota1[i] + nota2[i] + nota3[i] + nota4[i])/4)
+    print ('A media foi de: ', media)
 
-if media >= 7:
-    print('Aprovado')
-else:
-    print('Reprovado')'''
+    if media >= 7:
+        print('Status: Aprovado')
+
+    else:
+        print('Status: Reprovado')
