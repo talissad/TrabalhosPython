e070e9ea0c0aed8f3f2e54c1adf8e8d4dfe9563c
unknown <900161@proway.treina>
2019-11-19 08:24:49 -0300

    exercicio2

---
+++ Aula2/Exercicio2.py
@@ -0,0 +1,11 @@
+print('='*50, '\n'*3)
+
+nome = input('Digite seu nome:')
+sobrenome = input('Digite seu sobrenome:')
+cpf = input('Digite seu CPF:')
+rg = input('Digite seu RG:')
+salario = float(input('Digite seu salário:'))
+
+print('\n'*3)
+print('Nome: {}\nSobrenome: {}\nCPF: {}\nRG: {}\nSalário: {}'.format(nome, sobrenome, cpf, rg, salario))
+print('\n'*3, '='*50)
---
+++ Aula2/Exercicio4.py
@@ -0,0 +1,24 @@
+#--- O itineário deve conter cabeçalho com o título da viagem
+#--- Os dados de cada ponto devem estar em variáveis
+#--- Deve ser usado os caracteres de tabulação, quebra de linha e a função Format()
+
+
+pontoPartida = '7h30: Partida da Rua Gustavo Zimmermann'
+pontoChegada = '9h05: Chegada na Rua 7 de Setembro'
+pontoParada1 = '7h35 - parada nº 1 = Recanto dos Anjos'
+pontoParada2 = '7h40 - parada nº 2 = Eletrosul'
+pontoParada3 = '7h55 - parada nº 3 = Terminal da Itoupava'
+pontoParada4 = '8h10 - parada nº 4 = Rede top'
+pontoParada5 = '8h15 - parada nº 5 = Campo do atletico'
+pontoParada6 = '8h20 - parada nº 6 = Rancho do Soneca'
+pontoParada7 = '8h30 - parada nº 7 = Vale autoshopping'
+pontoParada8 = '8h40 - parada nº 8 = Terminal do Aterro'
+pontoParada9 = '8h55 - parada nº 9 = Giasse'
+pontoParada10 = '9h - parada nº 10 = Prefeitura'
+
+print('\n'*3)
+
+print('Ponto de partida: {}\nPonto de chegada: {}\nPrimeira parada: {}\nSegunda parada: {}\nTerceira parada: {}\nQuarta parada: {}\nQuinta parada: {}\nSexta parada: {}\nSetima parada: {}\nOitava parada: {}\nNona parada: {}\nDecima parada: {}\nChegada ao Destino'.format(
+pontoPartida, pontoChegada, pontoParada1, pontoParada2, pontoParada3, pontoParada4,pontoParada5, pontoParada6, pontoParada7, pontoParada8, pontoParada9, pontoParada10))
+
+print('\n'*3)
