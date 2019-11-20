b7d946d4bc12dfb307af099d78be24a57baacfcf
unknown <900161@proway.treina>
2019-11-20 11:51:29 -0300

    aula 10 - com alteração na formula  (dicionario)

--- Aula10/Aula10.py
+++ Aula10/Aula10.py
@@ -15,6 +15,7 @@ def calcular():
     n1 = int(request.args['numero1'])
     n2 = int(request.args['numero2'])
 
+
     r_soma= soma(n1,n2)
     r_subtracao = subtracao(n1,n2)
     r_multiplicacao = multiplicacao(n1,n2)
@@ -22,16 +23,12 @@ def calcular():
     r_divisao_fracionada = divisao_fracionada(n1,n2)
     r_resto = resto(n1,n2)
     r_raiz = raiz(n1,n2)
+    resultados = {'soma':r_soma, 'subtracao':r_subtracao, 'multiplicacao':r_multiplicacao, 'divisao_inteira':r_divisao_inteira, 'divisao_fracionada':r_divisao_fracionada, 'resto':r_resto, 'raiz':r_raiz}
 
     return render_template ('resultado.html'
     ,n1=n1
     ,n2=n2
-    ,soma=r_soma
-    ,subtracao=r_subtracao
-    ,divisao_inteira=r_divisao_inteira
-    ,divisao_fracionada=r_divisao_fracionada
-    ,resto=r_resto
-    ,raiz=r_raiz
+    ,resultados=resultados
     )
 
 app.run()
