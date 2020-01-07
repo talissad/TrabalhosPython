9a71f226f56b36e4ddfb51aec8475447632b0308
talissad <talissa.dahlke@gmail.com>
2020-01-07 15:46:24 -0300

    exercicios aula 29

--- Aula29/ex1HBA.py
+++ Aula29/ex1HBA.py
@@ -1,32 +1,82 @@
-# A HBSIS Airlines é uma empresa de aviação que opera rotas internacionais a partir de Blumenau.
-# Cada voo é tripulado por seis elementos, sendo que estes se dividem em dois grupos: a tripulação
-# técnica e a tripulação de cabine. A tripulação técnica é constituída pelo piloto e dois oficiais. 
-# A tripulação de cabine é constituída pelo chefe de serviço de voo e duas comissárias.
-# O transporte dos tripulantes entre o terminal e o avião é efetuado através de um Smart Fortwo, que
-# é um veículo que leva apenas duas pessoas. Por política da empresa, apenas o piloto e o chefe de
-# serviço de voo podem dirigir este veículo. É também política da empresa que nenhum dos oficiais
-# pode ficar sozinho com o chefe de serviço de bordo, e nem nenhuma das comissárias pode ficar
-# sozinha com o piloto.  No terminal de embarque estão os seis tripulantes e ainda um policial 
-# junto com um presidiário. Estes oito elementos terão que embarcar segundo as regras descritas acima. 
-# A empresa não coloca nenhum limite para o número de viagens entre o terminal e o avião.
-# Por motivos de segurança o presidiário não pode ficar sozinho em momento algum com os
-# tripulantes sem a presença do policial, nem no terminal e nem no avião. De forma a facilitar o
-# processo, a empresa autorizou que o policial pudesse dirigir o veículo também.
+# lista_motorista = ['piloto', 'chefe', 'policial']
+# lista_passageiro = ['presidiario', 'oficial 1', 'oficial 2', 'comissaria 1', 'comissaria 2']
 
-# Requisitos:
+#passageiroTerminal = ['piloto', 'chefe', 'policial', 'presidiario', 'oficial 1', 'oficial 2', 'comissaria 1', 'comissaria 2']
+passageiroTerminal = []
+passageiroAviao = []
 
-# 1 - Sempre que o Fortwo se mover, apresentar no console quando ele chega no destino
-# 2 - Sempre que acontecer um embarque, apresentar quais elementos estão embarcando
-# 3 - Sempre que acontecer um embarque no terminal, apresentar quem está no terminal
-# 4 - Sempre que acontecer um embarque no avião, apresentar quem está no avião
-# 5 - Deve ser feito em Python
+def adicionarPassageiro():
+    retorno = 's'
+    while retorno == 's':
+        opcao = input('Gostaria de adicionar alguém a viagem? (S/N) ')
+        if opcao == 's':
+            pessoaAdicionar = input('Digite o nome da pessoa a ser adicionada: ')
+            arquivo = open("Aula29/cadastro.txt", "a")
+            retorno = True
+        elif opcao == 'n':
+            break  
 
-lista_motorista = ['piloto', 'chefe', 'policial']
-lista_passageiro = ['pressidiario', 'oficial 1', 'oficial 2', 'comissaria 1', 'comissaria 2']
+def ler_arquivo():
+    arquivo = open("Aula29/cadastro.txt", "r")
+    for pessoas in arquivo:
+        pessoas = pessoas.strip().split()
+        passageiroTerminal.append(pessoas)
+    arquivo.close()
 
-passageiroTerminal = ['piloto', 'chefe', 'policial', 'pressidiario', 'oficial 1', 'oficial 2', 'comissaria 1', 'comissaria 2']
-passageiroFrotwo = []
-passageiroAvião = []
-motoristaAguardo = []
+def deslocamentoAviao(lista):
+    bool (passageiroTerminal)
+    while passageiroTerminal:
+        print(f"Passageiros esperando o deslocamento: {', '.join(passageiroTerminal)}")
+        motorista = input('Digite o motorista: ')
+        if motorista == 'piloto':
+            passageiro = input('Digite o passageiro: ')
+            if passageiro == 'chefe' or 'oficial 1' or 'oficial 2':
+                passageiroAviao.append(passageiro)
+                passageiroAviao.append(motorista)
+                passageiroTerminal.remove(motorista)
+                passageiroTerminal.remove(passageiro)
+                print(f'Fortwo está indo ao avião com o {motorista} e o {passageiro}')
+                print(f'Fortwo chegou ao avião\nDesembarcando o {motorista} e o {passageiro}')
+            else:
+                print('Opção inválida')
 
-def embarque
+        elif motorista == 'chefe':
+            passageiro = input('Digite o passageiro: ')
+            if passageiro == ('piloto' or 'comissaria 1' or 'comissaria 2'):
+                passageiroAviao.append(passageiro)
+                passageiroAviao.append(motorista)
+                passageiroTerminal.remove(motorista)
+                passageiroTerminal.remove(passageiro)
+                print(f'Fortwo está indo ao avião com o {motorista} e o {passageiro}')
+                print(f'Fortwo chegou ao avião, desembarcando o {motorista} e o {passageiro}')
+
+            else:
+                print('Opção inválida')
+
+        elif motorista == 'policial':
+            passageiro = 'presidiario'
+            passageiroAviao.append(motorista)
+            passageiroAviao.append(passageiro)
+            passageiroTerminal.remove(motorista)
+            passageiroTerminal.remove(passageiro)   
+            print(f'\nFortwo está indo ao avião com o {motorista} e o {passageiro}')
+            print(f'Fortwo chegou ao avião, desembarcando o {motorista} e o {passageiro}')
+
+        else:
+            print('Digite uma opção válida')
+
+        motoristaRetorno = input('\nDigite o motorista do retorno: ')
+        if motoristaRetorno == ('chefe' or 'piloto'):
+            passageiroAviao.remove(motoristaRetorno)
+            passageiroTerminal.append(motoristaRetorno)
+            print(f'Fortran retornando ao terminal com {motoristaRetorno}\n')
+        elif motoristaRetorno =='policial':
+            print('O policial não pode deixar o presidiario sozinho!')
+        elif motoristaRetorno == '0':
+            break       
+        else:
+            print('Somente o chefe e o piloto pode retornar')
+
+#adicionar = adicionarPassageiro()
+#inicio = ler_arquivo()
+opcoesUsuario = deslocamentoAviao(passageiroTerminal)
