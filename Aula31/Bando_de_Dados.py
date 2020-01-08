bb5679fb036418b874a809113b127f1486f9c58f
talissad <talissa.dahlke@gmail.com>
2020-01-08 11:53:01 -0300

    Aula banco de dados

---
+++ Aula31/Bando_de_Dados.py
@@ -0,0 +1,12 @@
+# Bando de dados
+# SGBD - Sistema Gerenciador de banco de dados
+# MySQL, SqlServer ...
+
+# Mysql = MariaDB
+
+# CRUD
+
+# CREATE        - insert
+# READ          - select
+# UPDATE        - update
+# DELETE        - delete
