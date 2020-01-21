6de4a036db41f9079f189901f3f4ae8e76fed9f3
talissad <talissa.dahlke@gmail.com>
2020-01-21 09:56:40 -0300

    Web / banco de dados

--- Aula36-copia/Controller/pessoa_controller.py
+++ Aula36-copia/Controller/pessoa_controller.py
@@ -10,10 +10,39 @@ class PessoaController:
     endereco_controller = EnderecoController()
 
     def listar_todos(self):
-        return self.dao.listar_todos()
+        lista_pessoas = []
+        lista_tuplas = self.dao.listar_todos()
+        for p in lista_tuplas:
+            pessoa = Pessoa()
+            pessoa.id =  p[0]
+            pessoa.nome = p[1]
+            pessoa.sobrenome = p[2]
+            pessoa.idade = p[3]
+            pessoa.endereco.id = p[5]
+            pessoa.endereco.logradouro = p[6]
+            pessoa.endereco.numero = p[7]
+            pessoa.endereco.complemento = p[8]
+            pessoa.endereco.bairro = p[9]
+            pessoa.endereco.cidade = p[10]
+            pessoa.endereco.cep = p[11]
+            lista_pessoas.append(pessoa)
+        return lista_pessoas
 
     def buscar_por_id(self, id):
-        return self.dao.buscar_por_id(id)
+        p = self.dao.buscar_por_id(id)
+        pessoa = Pessoa()
+        pessoa.id =  p[0]
+        pessoa.nome = p[1]
+        pessoa.sobrenome = p[2]
+        pessoa.idade = p[3]
+        pessoa.endereco.id = p[5]
+        pessoa.endereco.logradouro = p[6]
+        pessoa.endereco.numero = p[7]
+        pessoa.endereco.complemento = p[8]
+        pessoa.endereco.bairro = p[9]
+        pessoa.endereco.cidade = p[10]
+        pessoa.endereco.cep = p[11]
+        return pessoa
 
     def salvar(self, pessoa:Pessoa):
         pessoa.endereco.id = self.endereco_controller.salvar(pessoa.endereco)
@@ -24,4 +53,3 @@ class PessoaController:
 
     def deletar(self, id):
         self.dao.deletar(id)
-
--- Aula36-copia/View/web.py
+++ Aula36-copia/View/web.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sys
 sys.path.append('/Users/900161/Documents/TrabalhosPython/Aula36-copia/')
 from Controller.pessoa_controller import PessoaController
+from Controller.endereco_controller import EnderecoController
+from Model.endereco import Endereco
+from Model.pessoa import Pessoa
 
 app = Flask(__name__)
 pessoa_controller = PessoaController()
+end_controller = EnderecoController()
 nome = 'Cadastros'
+
 
 @app.route('/')
 def inicio():
@@ -18,6 +23,28 @@ def listar():
 
 @app.route('/cadastrar')
 def cadastrar():
-    return render_template('cadastrar.html', titulo_app = nome)
+    id = request.args['id']
+    pessoa = pessoa_controller.buscar_por_id(id)
+    return render_template('cadastrar.html', titulo_app = nome, pessoa = pessoa )
+
+
+@app.route('/salvar')
+def salvar():
+    pessoa = Pessoa()
+    pessoa.nome = request.args['nome']
+    pessoa.sobrenome = request.args['sobrenome']
+    pessoa.idade = request.args['idade']
+
+    end = Endereco()
+    end.logradouro = request.args['logradouro']
+    end.numero = request.args['numero']
+    end.complemento = request.args['complemento']
+    end.bairro = request.args['bairro']
+    end.cidade = request.args['cidade']
+    end.cep = request.args['cep']
+
+    pessoa.endereco = end
+    pessoa_controller.salvar(pessoa)
+    return redirect('/listar')
 
 app.run(debug=True)
