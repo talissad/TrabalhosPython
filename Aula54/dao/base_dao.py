3f6a930d42c5218eb1a4331a9699e182c0f568ae
talissad <talissa.dahlke@gmail.com>
2020-02-07 11:43:14 -0300

    Aula 53 e 54 - mysql connector/Alchemy

--- Aula54/dao/base_dao.py
+++ Aula54/dao/base_dao.py
@@ -3,9 +3,9 @@ import sqlalchemy as db
 
 class BaseDao:
     def __init__(self):
+        # Cria a conexao com o banco de dados
+        # (SGBD +conector ://usuario :senha @url :porta /nome da database
         conexao = db.create_engine("mysql+mysqlconnector://root:@127.0.0.1:3306/aulabd")
         criado_sessao = db.orm.sessionmaker()
         criado_sessao.configure(bind=conexao)
         self.sessao = criado_sessao()
-
-        # (host='127.0.0.1', database='aulabd', user='root', passwd='')
