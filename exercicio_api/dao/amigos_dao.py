bdaaa0ec70cad2b9e467b3179f43c656ea19ddb0
talissad <talissa.dahlke@gmail.com>
2020-02-17 10:21:17 -0300

    exercicio Api

--- exercicio_api/dao/amigos_dao.py
+++ exercicio_api/dao/amigos_dao.py
@@ -11,20 +11,25 @@ class AmigosDao:
         for i in self.lista_pessoas:
             print(i+'\n')
 
-    def get_by_id(self,  id):
-        if id in self.lista_pessoas:
-
-            print(id)
+    def get_by_id(self, id):
+        for i in self.lista_pessoas:
+            for j in i:
+                if id in j:
+                    return print(i)
 
     def insert(self):
         arquivo = open("C:/Users/900161/Documents/TrabalhosPython/exercicio_api/pessoas.txt", "a")
-        while retorno == 's':
+        while retorno == 'S':
             retorno = input('Gostaria de adicionar alguém a lista? (S/N) ')
-            if retorno == 's':
+            retorno.upper()
+            if retorno == 'S':
                 arquivo.write('\n'+input('Digite o nome da pessoa a ser adicionada: '))
             arquivo.close()
         return f"objeto(s) inserido(s) com sucesso!"
 
     def delete(self, id):
-        del(id)
+        arquivo = open("C:/Users/900161/Documents/TrabalhosPython/exercicio_api/pessoas.txt", "w")
+        for i in arquivo:
+            if i == id:
+                self.lista_pessoas.remove(id)
         return f"objeto deletado com sucesso!"
