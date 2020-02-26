bd27c49589cac481ecc8aebb9cb6b5ce2469d2a4
talissad <talissa.dahlke@gmail.com>
2020-02-26 11:54:51 -0300

    Aula 61 - Atividade Logica

---
+++ Aula 61/atividade_bin.py
@@ -0,0 +1,44 @@
+# Um espaço binário dentro de um número inteiro positivo N é qualquer sequência máxima de zeros consecutivos
+# que está rodeado por 1 dos dois lados na representação binária de N.
+#
+# Por exemplo, o número 9 tem representação binária 1001 e contém uma lacuna binária de comprimento 2.
+# O número 529 tem representação binária 1000010001 e contém duas lacunas binárias:
+# um de comprimento 4 e um de comprimento 3.
+# O número 20 tem representação binária 10100 e contém um espaço binário de comprimento 1.
+# O número 15 tem representação binária 1111 e não possui lacunas binárias.
+# O número 32 tem representação binária 100000 e não possui lacunas binárias.
+#
+# Escreva uma função:
+#
+# solução def (N)
+#
+# que, dado um número inteiro positivo N, retorna o comprimento de seu maior intervalo binário.
+# A função deve retornar 0 se N não contiver um espaço binário.
+#
+# Exemplo, dado N = 1041, a função deve retornar 5,
+# porque N tem representação binária 10000010001 e, portanto, seu maior intervalo binário tem o comprimento 5.
+#
+# Dado N = 32, a função deve retornar 0, porque N tem representação binária '100000'
+# e, portanto, sem lacunas binárias.
+
+
+def ler_binario(binario):
+    n = bin(binario)
+    binario_convetido = n[2:]
+
+    anterior = ''
+    lacuna = 0
+    for numero in binario_convetido:
+        int(numero)
+        if numero != anterior and anterior == '0':
+            lacuna += 1
+        anterior = numero
+
+    print(f'Convertido = {binario_convetido}')
+    print(f'Existe {lacuna} lacuna(s)\n')
+
+
+ler_binario(529)
+ler_binario(15)
+ler_binario(32)
+ler_binario(9)
